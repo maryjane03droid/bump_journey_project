a9feadb23fb9e47ccb1e2e71ef8e4fd747e79ddb
Maryjane Wakuthii <kuthiemaryjane@gmail.com>
2026-06-23 09:17:37 +0300

    install gunicorn

--- bump_journey/settings.py
+++ bump_journey/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',  # 🟢 Placed at the top to intercept frontend requests early
+    'corsheaders.middleware.CorsMiddleware',  #  intercept frontend requests early
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'bump_journey.urls'
