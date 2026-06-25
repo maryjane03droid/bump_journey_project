ba5bbd99590e871a6deea74991ea6cf4f4da92f4
Maryjane Wakuthii <kuthiemaryjane@gmail.com>
2026-06-25 11:03:28 +0300

    debug

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,19 +1,38 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from .models import PatientProfile
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
+# 1. REGISTER SERIALIZER (Base margin)
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'password', 'role', 'is_approved', 'email']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['id', 'username', 'email', 'password', 'role']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            role=validated_data.get('role', 'PATIENT'),
-            email=validated_data.get('email', '')
-        )
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)  # Hashes the password securely
+        user.save()
         return user
+
+
+# 2. PATIENT PROFILE SERIALIZER (Moved to base margin)
+class PatientProfileSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+    email = serializers.ReadOnlyField(source='user.email')
+
+    class Meta:
+        model = PatientProfile
+        fields = [
+            'id', 
+            'username', 
+            'email', 
+            'estimated_due_date', 
+            'current_week', 
+            'emergency_contact_name', 
+            'emergency_contact_phone'
+        ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CustomTokenObtainPairView, ApproveStaffView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import RegisterView, PatientProfileDetailView  # Added PatientProfileDetailView here
+
 
 urlpatterns = [
-    path('accounts/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('accounts/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('accounts/users/<int:pk>/approve/', ApproveStaffView.as_view(), name='approve_staff'),
+    # 1. Registration Endpoint
+    path('register/', RegisterView.as_view(), name='auth_register'),
+    
+    # 2. Login Endpoint (Returns access and refresh tokens automatically)
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    
+    # 3. Token Refresh Endpoint
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # 4. Profile Endpoint
+    path('profile/', PatientProfileDetailView.as_view(), name='patient_profile'),
+
+    
 ]
